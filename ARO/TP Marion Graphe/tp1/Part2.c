5cd4b5ce7306987b4c93f41dec98f41d4e189fcd
spirita <azariel@hotmail.fr>
2015-11-09 15:05:36 +0100

    aro graphe 1

--- ARO/TP Marion Graphe/tp1/Part2.c
+++ ARO/TP Marion Graphe/tp1/Part2.c
@@ -1,7 +1,7 @@
-/* Ce program prend un nom de graphe en entrée, le charge,
+/* Ce programme prend un nom de graphe en entrée, le charge,
    et affiche :
-	- la liste des sommets qui n'ont pas de voisins
-	- la  liste  des  sommets  qui  ont  le  plus  de  voisins
+   - la liste des sommets qui n'ont pas de voisins
+   - la  liste  des  sommets  qui  ont  le  plus  de  voisins
 */
 
 #include <stdlib.h>
@@ -12,19 +12,19 @@
 void
 max
 (int *a,int b){
-	if (b>a)
-		*a=b;
+  if (b>*a)
+    *a=b;
 }
 
 int main(int argc, char *argv[]) {
 
   tGraphe graphe;
 
-// Variables supplémentaires
-int nbSommet=0;
-int i;
-int maxVoisin=0;
-tNomSommet nom;
+  // Variables supplémentaires
+  int nbSommet=0;
+  int i;
+  int maxVoisin=0;
+  tNomSommet nom;
 
   if (argc<2) {
     halt("Usage : %s FichierGraphe\n", argv[0]);
@@ -35,27 +35,31 @@ tNomSommet nom;
   
   grapheChargeFichier(graphe,  argv[1]);
   
-/* Début code modifié */
-nbSommet=grapheNbSommets(graphe);
-for (i=0,i<nbSommet,i++){
-	int nbVi=grapheNbVoisinsSommet(graphe, i);
-	if (nbVi==0){
-		grapheRecupNomSommet(graphe, i, nom);
-		printf("%s n'a pas de voisin.\n", nom);
-	} else {
-		max(&maxVoisin,nbVi);
-	}
-}
-if (maxVoisin != 0){
-	for (i=0,i<nbSommet,i++){
-	int nbVi=grapheNbVoisinsSommet(graphe, i);
-	if (nbVi==maxVoisin){
-		grapheRecupNomSommet(graphe, i, nom);
-		printf("%s n'a pas de voisin.\n", nom);
-	}
-}
+  /* Début code modifié */
+  printf("\n Affichage des sommets n'ayant pas de voisins.\n");
+  nbSommet=grapheNbSommets(graphe);
+  for (i=0;i<nbSommet;i++){
+    int nbVi=grapheNbVoisinsSommet(graphe, i);
+    if (nbVi==0){
+      grapheRecupNomSommet(graphe, i, nom);
+      printf("%s n'a pas de voisin.\n", nom);
+    } else {
+      max(&maxVoisin,nbVi);
+    }
+  }
+  printf("\n Affichage des sommets ayant le nombre maximum de voisins.\n");
+  printf("Le nombre maximum de voisns est %i.\n", maxVoisin);
+  if (maxVoisin != 0){
+    for (i=0;i<nbSommet;i++){
+      int nbVi=grapheNbVoisinsSommet(graphe, i);
+      if (nbVi==maxVoisin){
+	grapheRecupNomSommet(graphe, i, nom);
+	printf("%s a le nombre maximum de voisns.\n", nom);
+      }
+    }
+  }
 
-/* Fin code modifié */
+  /* Fin code modifié */
 
   grapheLibere(graphe);
 
