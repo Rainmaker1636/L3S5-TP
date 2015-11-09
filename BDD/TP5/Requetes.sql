983136ca7b7b277922b74a3ba119d90668a3cd7f
Uryon <valentin.ryckewaert@gmail.com>
2015-11-09 16:55:12 +0100

    bdd tp5, jusque q10

--- BDD/TP5/Requetes.sql
+++ BDD/TP5/Requetes.sql
@@ -26,25 +26,77 @@ where salaire < prix2;
 drop view if exists volCheapCdgNou;
 
 --3)
-drop view if exists minPorteedePilotedePleindeuro;
+drop view if exists porteeMaxPilote;
 
-create view minPorteedePilotedePleindeuro as
-select min(portee) as portee2 from pilote
-group by eid,salaire
-having  salaire > 100000;
+create view porteeMaxPilote as
+select max(portee) as portee from pilote
+where salaire > 100000
+group by eid;
 
-select dep,arr from vols, minPorteedePilotedePleindeuro as pv2
-where not exists
-(select * from pv2
-where distance > portee2);
+select dep,arr,distance from vols
+where distance <= ALL
+(select portee from PorteeMaxPilote);
 
-drop view if exists minPorteedePilotedePleindeuro;
+drop view if exists porteeMaxPilote;
 
 --4)
 
+select enom from pilote
+group by eid, enom
+having EVERY(portee > 1500); 
 
+--5)
 
+select enom from pilote
+group by eid, enom
+having EVERY(portee > 1500) AND count(*) >=2;
 
+--6)
 
+select enom from pilote
+group by eid, enom
+having EVERY(portee > 1500) AND bool_or(anom LIKE '%Boeing%'); 
 
+--7)
+
+select enom,eid from employes e
+ORDER BY salaire DESC
+LIMIT 1 OFFSET 1;
+
+--8)
+
+select enom from pilote
+group by eid, enom
+having  bool_or(portee > 2000) and bool_and(anom not LIKE '%Boeing%');
+
+--9)
+
+drop view if exists moySalairePilote;
+drop view if exists uniquePilote;
+
+create view uniquePilote as
+select eid,salaire from pilote
+group by eid,salaire;
+
+create view moySalairePilote as
+select avg(salaire) as moySalaire from uniquePilote;
+
+(select enom, salaire FROM employes
+where salaire > ALL
+ (select moySalaire from moySalairePilote)
+)
+EXCEPT (select enom, salaire FROM pilote);
+
+--10)
+
+drop view if exists moySalaireAll;
+
+create view moySalaireAll as
+select avg(salaire) as moyAll from employes;
+
+select (moySalaire - moyAll) as diffMoy from moySalaireAll CROSS JOIN moySalairePilote;
+
+drop view if exists moySalaireAll;
+drop view if exists moySalairePilote;
+drop view if exists uniquePilote;
 
